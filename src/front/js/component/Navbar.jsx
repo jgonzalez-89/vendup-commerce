4a44dc8514a299aef46681b4a512225533d1923f
Jose L. Gonzalez Diaz <kbcerverjo@gmail.com>
2023-02-17 11:56:01 +0000

    Create ButtonUser.jsx

--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -1,34 +1,20 @@
-import React from "react";
-import logo from "../../../../public/logowhite.png";
-// import Login from "../pages/login.jsx";
-// import SignUp from "../pages/signup.jsx";
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
   return (
-    <>
-      <header className="p-3 bg-dark text-bg-dark">
-        <div className="container-fluid">
-          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a
-              href="/"
-              className="d-flex align-items-left mb-2 mb-lg-0 text-white text-decoration-none"
-            >
-              <img src={logo} width="100" role="img" aria-label="Vendup"></img>
-              <span className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start fs-4">
-                VENDUP
-              </span>
-            </a>
-
-            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0"></ul>
-
-            {/* <div className="text-end">
-          <button type="button" className="btn btn-outline-light me-2" onClick={Login} to="/login">Login</button>
-          <button type="button" className="btn btn-warning" onClick={SignUp} to="/signup">Sign-up</button>
-        </div> */}
-          </div>
-        </div>
-      </header>
-    </>
+    <Navbar bg="dark" variant="dark" style={{ minHeight: "64px" }}>
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
 
--- src/front/js/pages/userpage.jsx
+++ src/front/js/pages/userpage.jsx
@@ -7,8 +7,11 @@ import VentasComponent from "../component/VentasComponent.jsx";
 import FavoritosComponent from "../component/FavoritosComponent.jsx";
 import BuzonComponent from "../component/BuzonComponent.jsx";
 import AjustesComponent from "../component/AjustesComponent.jsx";
+import ButtonUser from "../component/ButtonUser.jsx";
 
 const Userpage = () => {
+  const userId = 12; // Aquí se especifica el ID del usuario
+
   const [userName, setUserName] = useState({});
   const handler = new HttpHandler();
 
@@ -24,7 +27,6 @@ const Userpage = () => {
   }, []);
 
   const [selectedButton, setSelectedButton] = useState("Compras");
-  const userId = 1; // Aquí se especifica el ID del usuario
 
   const handleButtonClick = (buttonName) => {
     setSelectedButton(buttonName);
@@ -52,150 +54,98 @@ const Userpage = () => {
       renderComponent = <div>No se encontró componente.</div>;
   }
 
+  const navbarHeight = 64;
   return (
     <>
       <Header />
-      <div style={{ display: "flex" }}>
-        <div style={{ width: "20vw", backgroundColor: "#EEEEEF" }}>
-          {/* Contenido del primer div */}
-          <div>
-            <div
-              className="d-flex flex-column flex-shrink-0 p-3 bg-light"
-              style={{ height: "100vh" }}
-            >
-              <a className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-                <svg
-                  className="bi pe-none me-2 justify-content-center"
-                  width="40"
-                  height="32"
-                >
+      <div className="container-fluid">
+        <div className="row">
+          <div
+            className="col-lg-3 col-md-4 col-sm-12"
+            style={{ height: `calc(100vh - ${navbarHeight}px)` }}
+          >
+            <div className="d-flex flex-column flex-shrink-0 p-3 bg-light">
+              <div className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                {/* <svg className="bi pe-none me-2 justify-content-center" width="40" height="32">
                   <use href="#bootstrap" />
-                </svg>
+                </svg> */}
 
                 <div className="flex-column">
                   <div className="row mx-auto">
-                    <img src={userName.pictures} alt="" />
+                    <img
+                      src={userName.pictures}
+                      alt=""
+                      className="rounded-circle"
+                    />
                   </div>
                   <div
                     className="row fs-4 mx-auto p-3"
-                    style={{ color: "#7F7D82" }}
+                    style={{ color: "black" }}
                   >
                     Bienvenido <br />
                     {userName.name + " " + userName.surnames}
                   </div>
                 </div>
-
-              </a>
+              </div>
               <hr />
-              <ul className="nav nav-pills flex-column mb-auto">
-                <li className="nav-item">
-                  <div className="d-flex justify-content-center">
-                    <button
-                      className={`nav-link ${
-                        selectedButton == "Compras" ? "active" : ""
-                      }`}
-                      aria-current="page"
-                      onClick={() => handleButtonClick("Compras")}
-                      style={{
-                        width: "80%",
-                        marginTop: "2vw",
-                        backgroundColor:
-                          selectedButton === "Compras" ? "#FEBD2F" : "",
-                        color: "#7F7D82",
-                      }}
-                    >
-                      Compras
-                    </button>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <div className="d-flex justify-content-center">
-                    <button
-                      className={`nav-link ${
-                        selectedButton == "Ventas" ? "active" : ""
-                      }`}
-                      aria-current="page"
-                      onClick={() => handleButtonClick("Ventas")}
-                      style={{
-                        width: "80%",
-                        marginTop: "2vw",
-                        backgroundColor:
-                          selectedButton === "Ventas" ? "#FEBD2F" : "",
-                        color: "#7F7D82",
-                      }}
-                    >
-                      Ventas
-                    </button>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <div className="d-flex justify-content-center">
-                    <button
-                      className={`nav-link ${
-                        selectedButton == "Favoritos" ? "active" : ""
-                      }`}
-                      aria-current="page"
-                      onClick={() => handleButtonClick("Favoritos")}
-                      style={{
-                        width: "80%",
-                        marginTop: "2vw",
-                        backgroundColor:
-                          selectedButton === "Favoritos" ? "#FEBD2F" : "",
-                        color: "#7F7D82",
-                      }}
-                    >
-                      Favoritos
-                    </button>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <div className="d-flex justify-content-center">
-                    <button
-                      className={`nav-link ${
-                        selectedButton == "Buzón" ? "active" : ""
-                      }`}
-                      aria-current="page"
-                      onClick={() => handleButtonClick("Buzón")}
-                      style={{
-                        width: "80%",
-                        marginTop: "2vw",
-                        backgroundColor:
-                          selectedButton === "Buzón" ? "#FEBD2F" : "",
-                        color: "#7F7D82",
-                      }}
-                    >
-                      Buzón
-                    </button>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <div className="d-flex justify-content-center">
-                    <button
-                      className={`nav-link ${
-                        selectedButton == "Ajustes" ? "active" : ""
-                      }`}
-                      aria-current="page"
-                      onClick={() => handleButtonClick("Ajustes")}
-                      style={{
-                        width: "80%",
-                        marginTop: "2vw",
-                        backgroundColor:
-                          selectedButton === "Ajustes" ? "#FEBD2F" : "",
-                        color: "#7F7D82",
-                      }}
-                    >
-                      Ajustes
-                    </button>
-                  </div>
-                </li>
-              </ul>
+              <div className="mipo">
+                <ul className="nav nav-pills flex-column mb-auto">
+                  <li className="nav-item">
+                    <div className="d-flex justify-content-center">
+                      <ButtonUser
+                        text="Compras"
+                        selected={selectedButton === "Compras"}
+                        handleClick={() => handleButtonClick("Compras")}
+                      />
+                    </div>
+                  </li>
+                  <li className="nav-item">
+                    <div className="d-flex justify-content-center">
+                      <ButtonUser
+                        text="Ventas"
+                        selected={selectedButton === "Ventas"}
+                        handleClick={() => handleButtonClick("Ventas")}
+                      />
+                    </div>
+                  </li>
+                  <li className="nav-item">
+                    <div className="d-flex justify-content-center">
+                      <ButtonUser
+                        text="Favoritos"
+                        selected={selectedButton === "Favoritos"}
+                        handleClick={() => handleButtonClick("Favoritos")}
+                      />
+                    </div>
+                  </li>
+                  <li className="nav-item">
+                    <div className="d-flex justify-content-center">
+                      <ButtonUser
+                        text="Buzón"
+                        selected={selectedButton === "Buzón"}
+                        handleClick={() => handleButtonClick("Buzón")}
+                      />
+                    </div>
+                  </li>
+                  <li className="nav-item">
+                    <div className="d-flex justify-content-center">
+                      <ButtonUser
+                        text="Ajustes"
+                        selected={selectedButton === "Ajustes"}
+                        handleClick={() => handleButtonClick("Ajustes")}
+                      />
+                    </div>
+                  </li>
+                </ul>
+              </div>
               <hr />
             </div>
           </div>
-        </div>
-        <div style={{ width: "80vw", backgroundColor: "#C4BFBD" }}>
-          {/* Contenido del segundo div */}
-          <div className="container">{renderComponent}</div>
+          <div
+            className="col-lg-9 col-md-8 col-sm-12"
+            style={{ maxHeight: "calc(100vh - 64px)", overflowY: "auto" }}
+          >
+            <div className="container">{renderComponent}</div>
+          </div>
         </div>
       </div>
     </>
