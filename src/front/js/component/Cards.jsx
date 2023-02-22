6ca870fee4ab8e23f4fea0abb1c5b358bd714d0a
INVIiICTUS <j.enriquealorenzo@gmail.com>
2023-02-22 11:27:42 +0000

    casi acabado

--- src/front/js/component/Cards.jsx
+++ src/front/js/component/Cards.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { HttpHandler } from "../../../http/handler.js";
+import { Card, Modal, ListGroup, Button } from "react-bootstrap";
 
 function Cards() {
   const [data, setData] = useState({});
@@ -17,25 +18,85 @@ function Cards() {
   console.log(data);
 
   return (
-    <div className="m-5">
-      <div className="container-xl">
-        <div className="row row-cols-1 row-cols-md-5 g-4">
+    <>
+      {!data || !data.product ? (
+        <div>
+          <h1>Cargando</h1>
+        </div>
+      ) : (
+        <div className="container">
           {data.product &&
             data.product.map((item, index) => (
-              <div className="card xl m-4 p-1" key={index}>
-                <img src={item.images} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h5 className="card-title">{item.name}</h5>
-                  <p className="card-text">{item.description}</p>
-                  <a href="" className="btn btn-primary">
-                    Ver mas
-                  </a>
-                </div>
-              </div>
+              <Card>
+                <Card.Img
+                  variant="top"
+                  src={data.images}
+                  style={{
+                    height: "200px",
+                    width: "100%",
+                    objectFit: "cover",
+                  }}
+                />
+                <Card.Body>
+                  <Card.Title>{data.name}</Card.Title>
+                  <Card.Text
+                    style={{
+                      height: "150px",
+                      maxHeight: "150px",
+                      overflow: "hidden",
+                    }}
+                  >
+                    {data.description}
+                  </Card.Text>
+                  <ListGroup className="list-group-flush">
+                    <hr />
+                    <ListGroup.Item>Precio: {data.price} €</ListGroup.Item>
+                  </ListGroup>
+                </Card.Body>
+                <Card.Footer className="d-flex justify-content-between align-items-center">
+                  {/* <small className="text-muted">
+                    {daysRemaining > 0
+                      ? `${daysRemaining} días y ${hoursRemaining} horas restantes`
+                      : "Venta Finalizada"}
+                  </small> */}
+                  {/* <Button
+                    variant="warning"
+                    onClick={() => handleEditClick(producto)}
+                  >
+                    Editar +
+                  </Button> */}
+                </Card.Footer>
+              </Card>
             ))}
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
+
 export default Cards;
+
+{
+  /* <div className="m-5">
+        <div className="container-xl">
+          <div className="row row-cols-1 row-cols-md-5 g-4">
+            {data.product &&
+              data.product.map((item, index) => (
+                <div className="card xl m-4 p-1" key={index}>
+                  <img src={item.images} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{item.name}</h5>
+                    <p className="card-text">{item.description}</p>
+                    <a href="" className="btn btn-primary">
+                      Ver mas
+                    </a>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+// } */
+}
