a1bf49eeaa1eabb6a4084074f8c856d28f13cb85
INVIiICTUS <j.enriquealorenzo@gmail.com>
2023-02-22 10:44:03 +0000

    2.2

--- src/front/js/component/Cards.jsx
+++ src/front/js/component/Cards.jsx
@@ -18,47 +18,18 @@ function Cards() {
     <div className="m-5">
       <div className="container-xl">
         <div className="row row-cols-1 row-cols-md-5 g-4">
-          <div className="card xl m-4 p-1">
-            <img src="" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title"></h5>
-              <p className="card-text"></p>
-              <a href="" className="btn btn-primary">
-                ir al anuncio
-              </a>
+          {data.map((item, index) => (
+            <div className="card xl m-4 p-1" key={index}>
+              <img src={item.image} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{item.title}</h5>
+                <p className="card-text">{item.description} </p>
+                <a href="" className="btn btn-primary">
+                  ir al anuncio
+                </a>
+              </div>
             </div>
-          </div>
-          <div className="card xl m-4 p-1">
-            <img src="" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title"></h5>
-              <p className="card-text"></p>
-              <a href="" className="btn btn-primary">
-                ir al anuncio
-              </a>
-            </div>
-          </div>
-          <div className="card xl m-4 p-1">
-            <img src="" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title"></h5>
-              <p className="card-text"></p>
-              <a href="" className="btn btn-primary">
-                ir al anuncio
-              </a>
-            </div>
-          </div>
-
-          <div className="card xl m-4 p-1">
-            <img src="" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title"></h5>
-              <p className="card-text"></p>
-              <a href="" className="btn btn-primary">
-                ir al anuncio
-              </a>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
