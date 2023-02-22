c3a194888bfa571f70e47ea7fce56cb9a63a2951
INVIiICTUS <j.enriquealorenzo@gmail.com>
2023-02-22 11:09:27 +0000

    llamada al back en el home

--- src/front/js/component/Cards.jsx
+++ src/front/js/component/Cards.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { HttpHandler } from "../../../http/handler";
+import { HttpHandler } from "../../../http/handler.js";
 
 function Cards() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
 
   const handler = new HttpHandler();
+
   useEffect(() => {
     async function fetchData() {
       const result = await handler.getProduct();
@@ -12,24 +13,26 @@ function Cards() {
     }
     fetchData();
   }, []);
+
   console.log(data);
 
   return (
     <div className="m-5">
       <div className="container-xl">
         <div className="row row-cols-1 row-cols-md-5 g-4">
-          {data.map((item, index) => (
-            <div className="card xl m-4 p-1" key={index}>
-              <img src={item.image} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">{item.title}</h5>
-                <p className="card-text">{item.description} </p>
-                <a href="" className="btn btn-primary">
-                  ir al anuncio
-                </a>
+          {data.product &&
+            data.product.map((item, index) => (
+              <div className="card xl m-4 p-1" key={index}>
+                <img src={item.images} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{item.name}</h5>
+                  <p className="card-text">{item.description}</p>
+                  <a href="" className="btn btn-primary">
+                    Ver mas
+                  </a>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </div>
